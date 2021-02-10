afd96bebef14a3a22e760efe68e6cbc64bc9eb90
pianojoel <63880971+pianojoel@users.noreply.github.com>
2021-02-10 15:59:29 +0100

    Created Details Page with basic functionality

---
+++ Grupp9WebbShop.Web/Pages/ProductDetails.cshtml.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grupp9WebbShop.Data;
+using Grupp9WebbShop.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Grupp9WebbShop.Web.Pages
+{
+    public class ProductDetailsModel : PageModel
+    {
+        private readonly IShopDataService _ds;
+        [BindProperty(SupportsGet = true)]
+        public int? ProductId { get; set; }
+        public Product Product { get; set; }
+        
+        public ProductDetailsModel(IShopDataService ds)
+        {
+            _ds = ds;
+        }
+
+        public void OnGet()
+        {
+            if (ProductId != null)
+            {
+                Product = _ds.GetProductById(ProductId.Value);
+            }
+
+        }
+    }
+}
