40b11f06da03aa0d79f41109431a839e8c8d37a2
NatasaGrujic <71067401+NatasaGrujic@users.noreply.github.com>
2021-02-15 16:39:38 +0100

    Basic cart page

---
+++ Grupp9WebbShop.Web/Pages/ShoppingBasket.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Grupp9WebbShop.Data.Models;
+using Grupp9WebbShop.Data;
+using Grupp9WebbShop.Web.Helpers;
+
+namespace Grupp9WebbShop.Web.Pages
+{
+    public class ShoppingBasketModel : PageModel
+    {
+        private readonly IShopDataService _ds;
+
+        public IEnumerable<Product> Products { get; set; }
+        public ShoppingBasket Basket { get; set; }
+        public ShoppingBasketModel(IShopDataService ds)
+        {
+                        _ds = ds;
+        }
+        public void OnGet()
+        {
+            Basket = BasketHelper.GetBasket(HttpContext.Session);
+                        Products =  _ds.GetProducts();
+        }
+    }
+}
