5cec25483ae365dd776dd5e77a8f4a2056e16478
pianojoel <63880971+pianojoel@users.noreply.github.com>
2021-02-23 15:57:06 +0100

    override error messages for identity

---
+++ Grupp9WebbShop.Web/Areas/Identity/Data/CustomIdentityErrorDescriber.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grupp9WebbShop.Web.Areas.Identity.Data
+{
+    public class CustomIdentityErrorDescriber : IdentityErrorDescriber
+    {
+        public override IdentityError DefaultError() { return new IdentityError { Code = nameof(DefaultError), Description = $"Ett okänt fel har inträffat." }; }
+        public override IdentityError ConcurrencyFailure() { return new IdentityError { Code = nameof(ConcurrencyFailure), Description = "Optimistiskt fel, objektet har blivit modifierat." }; }
+        public override IdentityError PasswordMismatch() { return new IdentityError { Code = nameof(PasswordMismatch), Description = "Felaktigt lösenord." }; }
+        public override IdentityError InvalidToken() { return new IdentityError { Code = nameof(InvalidToken), Description = "Ogiltigt token." }; }
+        public override IdentityError LoginAlreadyAssociated() { return new IdentityError { Code = nameof(LoginAlreadyAssociated), Description = "En användare med detta login existerar redan." }; }
+        public override IdentityError InvalidUserName(string userName) { return new IdentityError { Code = nameof(InvalidUserName), Description = $"Användarnamn '{userName}' är ogiltigt, får bara innehålla bokstäver och siffror ." }; }
+        public override IdentityError InvalidEmail(string email) { return new IdentityError { Code = nameof(InvalidEmail), Description = $"Email '{email}' ogiltigt." }; }
+        public override IdentityError DuplicateUserName(string userName) { return new IdentityError { Code = nameof(DuplicateUserName), Description = $"Användarnamn '{userName}' är redan taget." }; }
+        public override IdentityError DuplicateEmail(string email) { return new IdentityError { Code = nameof(DuplicateEmail), Description = $"Email '{email}' är redan taget." }; }
+        public override IdentityError InvalidRoleName(string role) { return new IdentityError { Code = nameof(InvalidRoleName), Description = $"Rollnamn '{role}' är ogiltigt." }; }
+        public override IdentityError DuplicateRoleName(string role) { return new IdentityError { Code = nameof(DuplicateRoleName), Description = $"Rollnamn '{role}' är redan taget." }; }
+        public override IdentityError UserAlreadyHasPassword() { return new IdentityError { Code = nameof(UserAlreadyHasPassword), Description = "Användare har redan satt lösenord" }; }
+        public override IdentityError UserLockoutNotEnabled() { return new IdentityError { Code = nameof(UserLockoutNotEnabled), Description = "Utelåsning är inte aktiverat för denna användare" }; }
+        public override IdentityError UserAlreadyInRole(string role) { return new IdentityError { Code = nameof(UserAlreadyInRole), Description = $"Användaren är redan i rollen '{role}'." }; }
+        public override IdentityError UserNotInRole(string role) { return new IdentityError { Code = nameof(UserNotInRole), Description = $"Användaren är inte i rollen '{role}'." }; }
+        public override IdentityError PasswordTooShort(int length) { return new IdentityError { Code = nameof(PasswordTooShort), Description = $"Lösenord måste vara minst {length} tecken." }; }
+        public override IdentityError PasswordRequiresNonAlphanumeric() { return new IdentityError { Code = nameof(PasswordRequiresNonAlphanumeric), Description = "Lösenord måste ha åtminstone ett alfanumeriskt tecken." }; }
+        public override IdentityError PasswordRequiresDigit() { return new IdentityError { Code = nameof(PasswordRequiresDigit), Description = "Lösenord måste ha åtminstone en siffra ('0'-'9')." }; }
+        public override IdentityError PasswordRequiresLower() { return new IdentityError { Code = nameof(PasswordRequiresLower), Description = "Lösenord måste ha åtminstone en gemen ('a'-'z')." }; }
+        public override IdentityError PasswordRequiresUpper() { return new IdentityError { Code = nameof(PasswordRequiresUpper), Description = "Lösenord måste ha åtminstone en versal ('A'-'Z')." }; }
+    }
+}
