5ce86d03f89220473cd02d8947743e561042400c
Mikael Holmgren <mikael.holmgren@iths.se>
2021-03-17 16:13:11 +0100

    Basic inventoryadmin

--- Grupp9WebbShop.Web/Areas/ShopAdmin/Pages/InventoryAdmin.cshtml.cs
+++ Grupp9WebbShop.Web/Areas/ShopAdmin/Pages/InventoryAdmin.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Grupp9WebbShop.Data;
+using Grupp9WebbShop.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,26 @@ namespace Grupp9WebbShop.Web.Areas.ShopAdmin.Pages
 {
     public class InventoryAdminModel : PageModel
     {
-        public void OnGet()
+        private readonly IShopDataService _ds;
+
+        public InventoryAdminModel(IShopDataService ds)
         {
+            _ds = ds;
         }
+        public IEnumerable<Product> Products { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public int? EditId { get; set; }
+        public IActionResult OnGet(int? id, int? q)
+        {
+            
+            if (id != null)
+            {
+                _ds.SetProductStock(id.Value, q.Value);
+                return RedirectToPage("./InventoryAdmin");
+            }
+            Products = _ds.GetProducts().OrderBy(o => o.Category.Name).ThenBy(t => t.Name);
+            return Page();
+        }
+
     }
 }
