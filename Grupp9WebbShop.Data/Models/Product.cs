cc7489a922b0e3d265520a67636bb2e5f55ad734
Mikael Holmgren <mikael.holmgren@iths.se>
2021-03-17 13:49:12 +0100

    Admin CRUD translations + general fixes in page titles and alt-text on pics

--- Grupp9WebbShop.Data/Models/Product.cs
+++ Grupp9WebbShop.Data/Models/Product.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Grupp9WebbShop.Data.Models
 {
     public class Product
@@ -12,19 +14,24 @@ namespace Grupp9WebbShop.Data.Models
         [Display(Name = "Produktnamn")]
         public string Name { get; set; }
         [StringLength(500)]
+        [Display(Name = "Beskrivning")]
         public string Description { get; set; }
         [StringLength(200)]
         public string Summary { get; set; }
-
+        [Display(Name = "Pris")]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
-        
+        [Display(Name = "Kategori")]
         public ProductCategory Category { get; set; }
         [Required]
         public int CategoryId { get; set; }
         [StringLength(40)]
+        [Display(Name = "Bild")]
         public string ImageFile { get; set; }
         [StringLength(80)]
+        [Display(Name = "Bildbeskrivning")]
         public string ImageDescription { get; set; }
+        [Display(Name = "Utvald")]
         public bool Highlighted { get; set; }
     }
 }
