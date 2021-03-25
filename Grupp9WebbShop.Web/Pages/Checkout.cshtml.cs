8e4b3495db7d201429bf2f385b25534320e13eb2
Mikael Holmgren <mikael.holmgren@iths.se>
2021-03-25 12:09:23 +0100

    Checkoutpage connected fronted to backend

--- Grupp9WebbShop.Web/Pages/Checkout.cshtml.cs
+++ Grupp9WebbShop.Web/Pages/Checkout.cshtml.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Grupp9WebbShop.Web.Helpers;
 using Grupp9WebbShop.Web.Areas.Identity.Data;
+using Grupp9WebbShop.Data.Models;
 
 namespace Grupp9WebbShop.Web.Pages
 {
@@ -24,10 +25,22 @@ namespace Grupp9WebbShop.Web.Pages
         public void OnGet()
         {
         }
+        [BindProperty]
+        public ShippingTypes Shipping { get; set; }
+        [BindProperty]
+        public PaymentTypes Payment { get; set; }
+        public ShoppingBasket Basket { get; set; }
         public async Task<IActionResult> OnPostAsync()
         {
-
-            return RedirectToPage("/");
+            Basket = BasketHelper.GetBasket(HttpContext.Session);
+            var userId = _um.GetUserId(User);
+            Order newOrder = _ds.CreateOrderFromBasket(Basket, userId, Shipping, Payment);
+            _ds.SaveOrder(newOrder);
+            foreach (var item in Basket.Items)
+            {
+                _ds.DecreaseProductStock(item.ProductId, item.Quantity);
+            }
+            return RedirectToPage("/Index");
         }
     }
 }
