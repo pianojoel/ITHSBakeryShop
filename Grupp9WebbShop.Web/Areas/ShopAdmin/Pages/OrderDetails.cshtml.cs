3ea80b33dba65bc41b22e4fa880b3c574861d65c
NatasaGrujic <71067401+NatasaGrujic@users.noreply.github.com>
2021-04-06 14:49:31 +0200

    Test

--- Grupp9WebbShop.Web/Areas/ShopAdmin/Pages/OrderDetails.cshtml.cs
+++ Grupp9WebbShop.Web/Areas/ShopAdmin/Pages/OrderDetails.cshtml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Grupp9WebbShop.Data;
 using Grupp9WebbShop.Data.Models;
+using Grupp9WebbShop.Web.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -15,14 +17,18 @@ namespace Grupp9WebbShop.Web.Areas.ShopAdmin.Pages
         public int OrderId { get; set; }
         public Order Order { get; set; }
         private readonly IShopDataService _ds;
+        private readonly UserManager<WebbShopUser> _userManager;
+        public Task<WebbShopUser> User { get; set; }
 
-        public OrderDetailsModel(IShopDataService ds)
+        public OrderDetailsModel(IShopDataService ds, UserManager<WebbShopUser> userManager)
         {
             _ds = ds;
+            _userManager = userManager;
         }
         public void OnGet()
         {
             Order = _ds.GetOrder(OrderId);
+            User = _userManager.FindByIdAsync(Order.UserID);
         }
         public IActionResult OnPost()
         {
