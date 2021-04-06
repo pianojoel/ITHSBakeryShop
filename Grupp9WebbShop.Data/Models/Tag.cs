dda71973a1004692519d71a116f6f6110e250b3b
Mikael Holmgren <mikael.holmgren@iths.se>
2021-04-06 13:49:37 +0200

    Tag Model

---
+++ Grupp9WebbShop.Data/Models/Tag.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupp9WebbShop.Data.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(40)]
+
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; } = new HashSet<Product>();
+
+    }
+}
